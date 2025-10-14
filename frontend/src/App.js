3349d887bb8cc6ff3845a59406b41e16767a13d9
samaurisam <sam@bidr.co>
2025-10-14 12:57:03 -0400

    general updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Avatar from "./components/Avatar"; // We'll create this
+import VoiceInterface from "./components/VoiceInterface"; // We'll create this
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        background: "#000",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Avatar />
+      <VoiceInterface />
     </div>
   );
 }
